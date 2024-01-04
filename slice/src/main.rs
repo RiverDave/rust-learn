c78d5396b397e18642ed2dc9912655a9abe23581
David Rivera <davidriverg@gmail.com>
2024-01-04 03:31:32 -0500

    Learnt a bit more specifically about slices

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,18 +1,41 @@
 fn main() {
-    println!("{}", first_word(&String::from("manri gol")));
+    // println!("{}", first_word(&String::from("ALA gol")));
+    
+    // let str = String::from("ALA gol");
+    // let size : (&str, usize) = first_word(&str);
+    // println!("{}, {}", size.0, size.1);
+
+    let word :String = String::from("Hola Mundo");
+
+    let word2 :&str = &word[..5];
+    println!("{}", word2);
+
+    let sensize = sentinel_char(&word, 'a');
+    println!("{}",sensize);
+    
 }
 
-fn first_word(wrd: &String)-> usize {
+fn first_word(wrd: &String)-> (&str, usize) {
     let bytes = wrd.as_bytes();
 
-    for(i, &item) in bytes.iter().enumerate(){
+    for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
+            return (&wrd[..i], i);
+        }
+    }
+    (&wrd[..], wrd.len())
+}
+
+//returns length of str until sentinel character is found
+fn sentinel_char(wrd: &String, sentinel: char) -> usize {
+
+    //used to be able to iterate through wrd
+    let bytes = wrd.as_bytes();
+
+    for (i, &index) in bytes.iter().enumerate(){
+        if index == sentinel as u8 {
             return i;
         }
     }
-    
-
-
-
-    wrd.len()
+    0
 }
